3a5f285fc5c7008ab7577a37b7653f3ada7eee5d
Archangel <jesse@archangel.wtf>
2025-11-05 14:56:21 +0100

    Add obsolete markers

--- Libraries/SPTarkov.Reflection/Patching/ModPatchCache.cs
+++ Libraries/SPTarkov.Reflection/Patching/ModPatchCache.cs
@@ -3,6 +3,7 @@ namespace SPTarkov.Reflection.Patching;
 /// <summary>
 ///     Cache of active patches for mod developers to use for compatibility reasons
 /// </summary>
+[Obsolete("Patches will be injectable through IEnumerable<IRuntimePatch> in SPT 4.1, making this redundant")]
 public static class ModPatchCache
 {
     private static readonly List<AbstractPatch> _activePatches = [];
@@ -22,6 +23,7 @@ public static class ModPatchCache
     /// <remarks>
     ///     This should never be called before PreSptLoad is completed, otherwise could be empty.
     /// </remarks>
+    [Obsolete("Patches will be injectable through IEnumerable<IRuntimePatch> in SPT 4.1, making this redundant")]
     public static IReadOnlyList<AbstractPatch> GetActivePatches()
     {
         // We're not exposing _activePatches so it cant be altered outside of this class. Do NOT implement this as a property.
@@ -38,6 +40,7 @@ public static class ModPatchCache
     /// <remarks>
     ///     This should never be called before PreSptLoad is completed, otherwise could be empty.
     /// </remarks>
+    [Obsolete("Patches will be injectable through IEnumerable<IRuntimePatch> in SPT 4.1, making this redundant")]
     public static List<string> GetActivePatchedMethodNames()
     {
         var result = new List<string>();
